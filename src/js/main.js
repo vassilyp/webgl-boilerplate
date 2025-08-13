caea24217f113ece234c9003b6ae476473454981
Vassily P <vasspetrousevitch@gmail.com>
2025-08-13 16:02:52 -0700

    used twgl to minimize boilerplate

---
+++ src/js/main.js
@@ -0,0 +1,50 @@
+import * as twgl from './twgl-full.module.js'
+
+function main(vs, fs) {
+  const gl = document.getElementById('c').getContext('webgl2')
+  const programInfo = twgl.createProgramInfo(gl, [vs, fs])
+  twgl.setDefaults({ attribPrefix: 'a_' })
+
+  const arrays = {
+    position: {
+      numComponents: 2,
+      data: [0, 0, 0, 0.5, 0.7, 0],
+    },
+  }
+  const bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays)
+
+  function render(time) {
+    twgl.resizeCanvasToDisplaySize(gl.canvas)
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
+
+    const uniforms = {
+      time: time * 0.001,
+      resolution: [gl.canvas.width, gl.canvas.height],
+    }
+
+    gl.useProgram(programInfo.program)
+    twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo)
+    twgl.setUniforms(programInfo, uniforms)
+    twgl.drawBufferInfo(gl, bufferInfo)
+
+    requestAnimationFrame(render)
+  }
+  requestAnimationFrame(render)
+}
+
+const readShaderFiles = async () => {
+  const vertexShaderSource = await fetch('src/glsl/vert.glsl')
+    .then((res) => res.text())
+    .catch((error) => console.error(error))
+
+  const fragmentShaderSource = await fetch('src/glsl/frag.glsl')
+    .then((res) => res.text())
+    .catch((error) => console.error(error))
+
+  return [vertexShaderSource, fragmentShaderSource]
+}
+
+// Read in the shader files, and use them to run the webgl code
+await readShaderFiles().then((res) => {
+  main(res[0], res[1])
+})
